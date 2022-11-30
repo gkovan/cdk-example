ebad905d2a9fca00ef120dd77c376a2665f3446b
Gerry Kovan <gkovan@hotmail.com>
2022-11-30 18:13:50 -0500

    use CodePipelineSource.connection

--- src/main/java/com/myorg/MyPipelineStack.java
+++ src/main/java/com/myorg/MyPipelineStack.java
@@ -7,6 +7,7 @@ import software.amazon.awscdk.StackProps;
 import java.util.Arrays;
 import software.amazon.awscdk.pipelines.CodePipeline;
 import software.amazon.awscdk.pipelines.CodePipelineSource;
+import software.amazon.awscdk.pipelines.ConnectionSourceOptions;
 import software.amazon.awscdk.pipelines.ShellStep;
 
 public class MyPipelineStack extends Stack {
@@ -20,7 +21,7 @@ public class MyPipelineStack extends Stack {
         CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
         .pipelineName("MyPipeline")
         .synth(ShellStep.Builder.create("Synth")
-           .input(CodePipelineSource.gitHub("gkovan/cdk-example", "main"))
+           .input(CodePipelineSource.connection("gkovan/cdk-example", "main", ConnectionSourceOptions.builder().connectionArn("arn:aws:codestar-connections:us-east-1:698566732460:connection/58227ce1-f364-4649-aa6e-91ddf7c4e875").build()))
            .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
            .build())
         .build();
